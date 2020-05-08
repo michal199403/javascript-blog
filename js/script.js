e6713ec649c6064f38d7396a40ba162b9c1b7b41
Haraburda Michał <mich1l199403@gmail.com>
2020-05-08 20:33:02 +0200

    Adding class 'active' to links

--- js/script.js
+++ js/script.js
@@ -4,16 +4,20 @@
 //  });
 'use strict';
 const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log(event);
-  
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
+  console.log(event);
+    //console.log('clickedElement (with plus): ' + clickedElement);
+    
     /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
     /* add class 'active' to the clicked link */
-  
+  clickedElement.classList.add('active');
+  console.log('clickedElement: ', clickedElement);
     /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
   for(let activeArticle of activeArticles){
