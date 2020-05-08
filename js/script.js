cf5701c639136be6391cd5a89d530f6875f8314f
Haraburda Michał <mich1l199403@gmail.com>
2020-05-08 21:21:41 +0200

    Finishing 'titleClickHandler'

--- js/script.js
+++ js/script.js
@@ -15,19 +15,27 @@ const titleClickHandler = function(event){
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
+
     /* add class 'active' to the clicked link */
   clickedElement.classList.add('active');
   console.log('clickedElement: ', clickedElement);
+
     /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
+
     /* get 'href' attribute from the clicked link */
-  
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('article href:' , articleSelector);
+
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('Target article:', targetArticle);
+
     /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
